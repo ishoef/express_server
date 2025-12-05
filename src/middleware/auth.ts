e67934729395134add3c18db8f73e57317fbd5e3
ishoef <ismailhossennayeb@gmail.com>
2025-12-05 23:57:55 +0200

    update middleware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -3,6 +3,7 @@ import { NextFunction, Request, Response } from "express";
 const auth = () => {
   return (req: Request, res: Response, next: NextFunction) => {
     console.log("i am clickd");
+    console.log(req.body);
   };
 };
 
