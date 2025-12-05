9ebf356bd75834a2653fcdc697e1729ad9ba3903
ishoef <ismailhossennayeb@gmail.com>
2025-12-05 23:58:51 +0200

    get the token in the middleware

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 
 const auth = () => {
   return (req: Request, res: Response, next: NextFunction) => {
+    const token = req.headers.authorization;
     console.log("i am clickd");
     console.log(req.body);
     console.log(req.headers);
