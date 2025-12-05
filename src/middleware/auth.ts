760a52f8ed7eab390efd774ecfbfd739d50cdccc
ishoef <ismailhossennayeb@gmail.com>
2025-12-05 23:57:03 +0200

    added middleware for authentication

---
+++ src/middleware/auth.ts
@@ -0,0 +1,9 @@
+import { NextFunction, Request, Response } from "express";
+
+const auth = () => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    console.log("i am clickd");
+  };
+};
+
+export default auth;
--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { authController } from "./auth.controller";
+import auth from "../../middleware/auth";
 
 const router = Router();
 
 // http://localhost:5000/auth/login
-router.post("/login", authController.loginUser);
+router.post("/login", auth, authController.loginUser);
 
 export const authRouter = router;
